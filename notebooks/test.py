a4c5dcf8071aea8e81622e05e1ff42f49b9b6b6f
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-09-18 15:23:32 +0200

    implementing Poisson Noise

--- notebooks/test.py
+++ notebooks/test.py
@@ -1,8 +1,7 @@
-import cv2 as cv
-from matplotlib import pyplot as plt
 import numpy as np
 
-img_location: str = '../images/img_2048x2494x15.jpg'
-img = cv.imread(img_location)
-cv.imshow('lungs', img)
-cv.waitKey(0)
+
+lambdas = [x for x in range(1, 12, 3)]
+
+for lbd in lambdas:
+    print(lbd)
